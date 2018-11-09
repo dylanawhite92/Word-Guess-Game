1b8c939185007c43d559c2eb0747ec93bdb76251
Narwhal Nancy <dylanawhite11@gmail.com>
2018-11-09 13:20:04 -0600

    started js variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+// Array of word variables for the game
+var wizard = ["w", "i", "z", "a", "r", "d" ];
+var wand = ["w", "a", "n", "d"];
+var fireball = ["f", "i", "r", "e", "b", "a", "l", "l"];
+var crystal = ["c", "r", "y", "s", "t", "a", "l"];
+var arcane = ["a", "r", "c", "a", "n", "e"];
+var mage = ["m", "a", "g", "e"];
